e3297740af45715563e58f475fd2e8d4377ac6b2
valkyrie_pilot <valk@randomairborne.dev>
2024-04-14 11:00:20 -0600

    Add doc example

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 //! # ExpiringMap
 //! [`ExpiringMap`] is a wrapper around AHashMap that allows the specification
 //! of TTLs on entries. It does not support iteration.
+//!
+//! ```rust
+//! use std::time::Duration;
+//! use expiringmap::ExpiringMap;
+//! let mut map = ExpiringMap::new();
+//! map.insert("key", "value", Duration::from_millis(50));
+//! std::thread::sleep(Duration::from_millis(60));
+//! assert!(map.get(&"key").is_none())
+//! ```
 use std::{
     hash::Hash,
     ops::{Deref, DerefMut},
